2fcbd5d222f5a748a5eebf1c45ec720392926a41
GSarpong4423 <sarpongg@gmail.com>
2020-09-01 15:41:27 -0400

    Created react pages and added basic functions

---
+++ src/components/ChoreList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './ChoreList.css';
+
+const ChoreList = () => {
+    return (
+      <ul className="chore-list">
+        <li>Wash the Dishes</li>
+        <li>Clean the kitchen</li>
+        <li>CLean the bathroom</li>
+      </ul>
+  );
+};
+
+export default ChoreList;
